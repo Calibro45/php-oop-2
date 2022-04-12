c10a01c38ec4da51041101a96e187e5224948fdb
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-12 14:53:26 +0200

    bozza schema

--- index.php
+++ index.php
@@ -4,3 +4,36 @@
 L’utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
 Il pagamento avviene con la carta di credito, che non deve essere scaduta. */
 
+// prodotti :
+// nome-prodotto
+// categoria
+// prezzo
+// sconto
+
+// utenti:
+// nome
+// cognome
+// indirizzo
+// metodi pagamenti
+// registrazione si/no
+
+// login :
+// user-name
+// email
+// password
+
+// carrello :
+// prodotto
+// quantità
+// totale
+
+// ordini :
+// n. ordine
+// data consegna
+// totale ordine
+
+// pagamenti :
+// n.carta
+// data scadenza
+// tipologia (visa)
+// cvv
