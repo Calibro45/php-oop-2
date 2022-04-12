49ce1b6a6086e54a91646abed03656b431c423c2
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-12 15:55:11 +0200

    extend prova

--- logins.php
+++ logins.php
@@ -5,13 +5,15 @@
 // email
 // password
 
-class Logins {
+require_once __DIR__ . '/utenti.php';
+class Logins extends Utenti {
 
     public $userName;
     public $email;
     public $password;
 
-    public function __construct(string $_userName, string $_email, string $_password) {
+    public function __construct(string $nome, string $cognome, string $_userName, string $_email, string $_password) {
+        parent::__construct($nome, $cognome);
         $this -> userName = $_userName;
         $this -> email = $_email;
         $this -> password = $_password;
