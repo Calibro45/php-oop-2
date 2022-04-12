551ad954475156bfdb3a4f477506e2127f42aacd
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-12 15:22:55 +0200

    bozza utente

--- index.php
+++ index.php
@@ -41,6 +41,24 @@ var_dump($prodotto1);
 // registrazione si/no
 // sconto
 
+class Utenti {
+
+    public $nome;
+    public $cognome;
+    public $indirizzo;
+    public $metodiPagamento;
+    public $registrazione = false;
+    public $sconto;
+
+    public function __construct(string $_nome, string $_cognome) {
+        $this -> nome = $_nome;
+        $this -> cognome = $_cognome;
+    }
+};
+
+$utente1 = new Utenti('nico', 'il sardo');
+var_dump($utente1);
+
 // login :
 // user-name
 // email
