8f0af14972965cf864df1957a79c0a2fd84b06d0
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-13 12:01:15 +0200

    calc prezzo

--- carrello.php
+++ carrello.php
@@ -11,17 +11,33 @@ require_once __DIR__. '/prodotti.php';
 class Carrello extends Prodotti {
 
     public $articoli = [];
-    public $totaleCarrello;
+    private $totaleCarrello;
 
     public function __construct(string $prodotto, string $_categoria, int $_prezzo, int $_quantita) {
         $this -> addProdotto($prodotto, $_categoria, $_prezzo, $_quantita);
+        $this -> setTotale();
     }
 
     public function addProdotto($nome, $categoria, $prezzo, $quantita) {
         array_push($this -> articoli, new Prodotti($nome, $categoria, $prezzo, $quantita));
+        $this -> setTotale();
     }
 
+    public function setTotale() {
+        $totale = 0;
+
+        foreach ($this -> articoli as $prodotto) {
+            $quantita = $prodotto -> quantita;
+            $prezzo = $prodotto -> prezzo;
+
+            if ($quantita > 1) {
+                $totale += $prezzo * $quantita; 
+            } else {
+                $totale += $prezzo;
+            }
+        }
+
+        $this -> totaleCarrello = $totale;
+    }
     
-
-
 }
